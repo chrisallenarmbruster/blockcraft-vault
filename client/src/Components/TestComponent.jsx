d31aab7ee8c5d1ec559fd6e3727ebd027832ada1
Chris Armbruster <chris@armbrustermail.com>
2024-05-07 15:15:55 -0500

    Add Data slice, Refactor /api/vault routes to return DB response.

--- client/src/Components/TestComponent.jsx
+++ client/src/Components/TestComponent.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { processCredentials, getItem } from "../store/cryptoMemStore";
+import { processCredentials } from "../store/cryptoMemStore";
 import { login, logout, register } from "../store/authSlice";
 
 function TestComponent() {
