17f5765064335634a9e9a2762143ce65b81b09f9
Chris Armbruster <chris@armbrustermail.com>
2024-05-12 11:27:15 -0500

    Add custom.css for style overrides

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
